42fbba27e5c60d11d3c97bdf6181756d2c6a6f7a
Jeff Abrahamson <jeff@purple.com>
2013-05-03 21:57:39 +0100

    Bash install adds includes as necessary to real dot files.
    Resolves #3 for bash.

--- bash/install.sh
+++ bash/install.sh
@@ -18,3 +18,19 @@ maybe_mkdir $HOME/bash/
 	install_to $force $f $HOME/bash/;
     done
 )
+
+
+maybe_append()
+{
+    include_from=$1
+    include_this=$2
+    include_template=$3
+    grep -q HOME/$include_this $HOME/$include_from 2> /dev/null
+    if [ 0 != $? ]; then
+	cat $include_template >> $HOME/$include_from
+    fi
+}
+
+maybe_append .bashrc bash/rc bashrc-include
+maybe_append .bash_profile bash/profile bash_profile-include
+maybe_append .bash_logout bash/logout bash_logout-include
