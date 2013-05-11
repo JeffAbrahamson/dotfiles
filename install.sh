852c8526e9f86ba17c5b243b674689d8a0f8fb89
Jeff Abrahamson <jeff@purple.com>
2013-05-11 07:44:44 +0100

    Main install and test from root directory, where they are visible.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd $(pwd)/$(dirname $0)
+(echo bash; cd bash  && ./install.sh)
+(echo bin; cd bin   && ./install.sh)
+(echo emacs; cd emacs && ./install.sh)
--- script/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $(pwd)/$(dirname $0)
-(echo bash; cd ../bash  && ./install.sh)
-(echo bin; cd ../bin   && ./install.sh)
-(echo emacs; cd ../emacs && ./install.sh)
