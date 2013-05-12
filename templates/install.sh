6e0561a63d00d2f87a4eab4defb458cc6d4eada0
Jeff Abrahamson <jeff@purple.com>
2013-05-12 21:41:50 +0100

    Actually install template files.

---
+++ templates/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. ../script/lib.sh
+
+maybe_mkdir $HOME/templates
+(
+    # Install the template files
+    cd templates
+    for f in *; do
+	install_to $f $HOME/templates/;
+    done
+)
+
+(
+    # If I have private template files, install them as well
+    templates=$HOME/common/templates
+    if [ -d $templates ]; then
+	cd $templates
+	for f in *; do
+	    install_to $f $HOME/templates;
+	done
+    fi
+)
