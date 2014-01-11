57046d5cb14b63d9c46b5df8bb1f8c5710b1d64f
Jeff Abrahamson <jeff@purple.com>
2014-01-11 19:41:13 +0000

    Make sure screen locks on lid close.
    Also, some notes on how to suspend the computer on lid close,
    with a script option to do it when I need it.  (But suspend
    must run as root, so ratpoison can't itself do the suspend.)
    
    At ratpoison startup, launch the screen lock daemon as well as
    a terminal and an emacs session.

---
+++ ratpoison/ratpoison/acpi-local-lid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## On at least one machine, I needed this script to run pm-suspend on lid close.
+
+export PATH=/usr/bin:/bin
+
+log=/tmp/lid-log.txt
+date >> $log
+echo "Suspending from /etc/acpi/local/lid.sh.pre." >> $log
+
+# acpi hasn't changed the lid state yet, so run in a moment.
+(sleep 2; /home/jeffab/bin/ratpoison-local.mellinet once suspend) &
