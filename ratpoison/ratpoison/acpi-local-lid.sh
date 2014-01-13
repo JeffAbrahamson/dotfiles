b6a16bcf758fe1cbd72ee51e9a18bb9c839fe551
Jeff Abrahamson <jeff@purple.com>
2014-01-13 10:22:38 +0000

    Fix suspend wrapper script name

--- ratpoison/ratpoison/acpi-local-lid.sh
+++ ratpoison/ratpoison/acpi-local-lid.sh
@@ -9,4 +9,4 @@ date >> $log
 echo "Suspending from /etc/acpi/local/lid.sh.pre." >> $log
 
 # acpi hasn't changed the lid state yet, so run in a moment.
-(sleep 2; /home/jeffab/bin/ratpoison-local.mellinet once suspend) &
+(sleep 2; /home/jeffab/bin/ratpoison-lid-monitor once suspend) &
