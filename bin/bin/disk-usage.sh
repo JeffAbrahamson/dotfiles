0beb4fdc3b3fc0bb6d6e0366e9c987c07c293116
Jeff Abrahamson <jeff@purple.com>
2015-03-20 09:40:00 +0100

    Monitoring scripts to track uptime, disk usage, and network bandwidth.
    Add untracked script from fileserver to track disk usage on fileserver.
    
    Wrapper script for speedtest_cli.py to track network bandwidth
    (upload and download speeds and ping time).
    
    Update up-monitor script to be compatible with my current data
    collection strategies.  Mostly, better file naming, using a single
    file rather than daily files, and tracking time in seconds since the
    epoch.

---
+++ bin/bin/disk-usage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Monitor disk usage on fileserver.
+# This is a bit hard-coded.
+
+# Return size in megabytes for partition $1.
+get_size()
+{
+    df -BM "$1" | grep "$1" | awk '{print $3}' | tr -d M
+}
+
+# Write time and size in megabytes to partition $1 to datafile $2.
+record_size()
+{
+    now=$(date +%s)
+    size=$(get_size "$1")
+    echo $now $size >> "$2"
+}
+
+record_size /d1 $HOME/data/d1
+record_size /d2 $HOME/data/d2
