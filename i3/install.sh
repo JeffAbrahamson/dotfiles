c8453bb06c43246de2fb07ee07e9bfb709a94d0a
Jeff Abrahamson <jeff@purple.com>
2015-12-30 13:57:39 +0100

    Start dunst reliably.

--- i3/install.sh
+++ i3/install.sh
@@ -5,7 +5,8 @@
 dest=$HOME/.i3/
 maybe_mkdir $(dirname $dest)
 (cd i3/ && copy_to config $dest)
-(cd i3/ && copy_to dunstrc $dest)
+maybe_mkdir $HOME/.config/dunst
+(cd i3/ && copy_to dunstrc $HOME/.config/dunst/)
 
 dest=$HOME/bin/
 maybe_mkdir $dest
