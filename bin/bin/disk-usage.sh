6779e48c85540578adf6e17294b47e877696ff56
Jeff Abrahamson <jeff@purple.com>
2016-04-16 11:36:02 +0200

    Record remaining space on disk.

--- bin/bin/disk-usage.sh
+++ bin/bin/disk-usage.sh
@@ -9,12 +9,20 @@ get_size()
     df -BM "$1" | grep "$1" | awk '{print $3}' | tr -d M
 }
 
+# Return space remaining in megabytes for partition $1.
+get_remaining()
+{
+    df -BM "$1" | grep "$1" | awk '{print $4}' | tr -d M
+}
+
 # Write time and size in megabytes to partition $1 to datafile $2.
 record_size()
 {
     now=$(date +%s)
     size=$(get_size "$1")
+    remaining=$(get_remaining "$1")
     echo $now $size >> "$2"
+    echo $now $remaining >> "$2"-remaining
 }
 
 record_size /d1 $HOME/data/d1
