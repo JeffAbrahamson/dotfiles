c5d8bbf89075496dab47691a14b253c93441b118
Jeff Abrahamson <jeff@purple.com>
2016-12-24 18:40:02 +0100

    Customise i3statusbar

--- i3/install.sh
+++ i3/install.sh
@@ -3,6 +3,7 @@
 . ../script/lib.sh
 
 dest=$HOME/.i3/
+(cd i3/ && copy_to i3status.conf $HOME/.i3status.conf)
 maybe_mkdir $(dirname $dest)
 (cd i3/ && copy_to config $dest)
 maybe_mkdir $HOME/.config/dunst
