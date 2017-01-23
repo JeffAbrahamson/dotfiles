ce729bd77ee7613eb7155f8d7d99d4439a7dbf56
Jeff Abrahamson <jeff@purple.com>
2017-01-23 07:16:47 +0100

    Customise dunst font size on install

--- i3/install.sh
+++ i3/install.sh
@@ -7,7 +7,13 @@ dest=$HOME/.i3/
 maybe_mkdir $(dirname $dest)
 (cd i3/ && copy_to config $dest)
 maybe_mkdir $HOME/.config/dunst
-(cd i3/ && copy_to dunstrc $HOME/.config/dunst/)
+if [ "X$HOSTNAME" == "Xbirdsong" ]; then
+    (cd i3/ && sed -e 's/{% fontsize %}/10/;' < dunstrc > $HOME/.config/dunst/dunstrc)
+elif [ "X$HOSTNAME" == "Xstarshine" ]; then
+    (cd i3/ && sed -e 's/{% fontsize %}/18/;' < dunstrc > $HOME/.config/dunst/dunstrc)
+else
+    echo "Unrecognized host, not copying dunstrc."
+fi
 
 dest=$HOME/bin/
 maybe_mkdir $dest
