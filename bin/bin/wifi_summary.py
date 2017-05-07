3abbf7f47fa88a50b8e55d813785b18ca02a1378
Jeff Abrahamson <jeff@purple.com>
2017-05-07 11:38:58 +0100

    Add option to filter wifi analysis at the commandline

--- bin/bin/wifi_summary.py
+++ bin/bin/wifi_summary.py
@@ -3,6 +3,7 @@
 """Display a visualsiation of performance of a wifi SSID.
 """
 
+import argparse
 import os
 import pandas as pd
 #import matplotlib
@@ -11,7 +12,7 @@ import matplotlib.pyplot as plt
 #import pyplot as plt
 import pylab
 
-def get_data():
+def get_data(ssid):
     """Assemble wifi data into a pandas DataFrame.
     """
     data_dir_base = os.getenv('HOME') + '/data/hosts/'
@@ -27,22 +28,29 @@ def get_data():
     df = df_ping.set_index('timestamp').join(df_download.set_index('timestamp'), how='inner')
     df = df.join(df_upload.set_index('timestamp'), how='inner')
     df = df.join(df_ssid.set_index('timestamp'), how='inner')
-    df = df.loc[df.ssid == 'bguest']
+    if ssid != '':
+        df = df.loc[df.ssid == ssid]
     plt.scatter(df.upload, df.download, s=20 * df.ping)
     plt.xlabel("Upload speed")
     plt.ylabel("Download speed")
-    plt.title("bguest wifi performance (point size is ping time, larger is worse)")
+    plt.title("{ssid} wifi performance (point size is ping time, larger is worse)".format(
+        ssid=ssid))
     #pylab.show()
     gc = plt.gcf()
     xdim = 16
     ydim = 12
     gc.set_size_inches(xdim, ydim)
-    gc.savefig('/tmp/bguest.png', dpi=196)
+    gc.savefig('/tmp/wifi-{ssid}.png'.format(ssid=ssid), dpi=196)
 
 def main():
     """Gather arguments and plot.
     """
-    get_data()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ssid', type=str, required=False,
+                        default='',
+                        help='if present, filter to this ssid')
+    args = parser.parse_args()
+    get_data(args.ssid)
 
 if '__main__' == __name__:
     main()
