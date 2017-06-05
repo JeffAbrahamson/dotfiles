4971979dee986880f91683a77c79929cef122a26
Jeff Abrahamson <jeff@purple.com>
2017-06-05 20:44:54 +0200

    Note that port udp/5353 might be needed for printer discovery

--- firewall/install.sh
+++ firewall/install.sh
@@ -6,6 +6,7 @@ echo "Setting up firewall, will require sudo."
 sudo ufw default allow outgoing
 sudo ufw default deny incoming
 sudo ufw allow ssh/tcp
+#sudo ufw allow 5353/udp		# For hp-5540 printer discovery (?)
 sudo ufw limit ssh
 sudo ufw enable
 if [ "X$$HOSTNAME" = "Xsiegfried" ]; then
