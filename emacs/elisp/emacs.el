f4b12cd5b9a213d6e7e77987c72ebd58093538a9
Jeff Abrahamson <jeff@p27.eu>
2019-11-02 19:45:57 +0100

    Load auctex
    For some reason this isn't happening automatically.

--- emacs/elisp/emacs.el
+++ emacs/elisp/emacs.el
@@ -17,26 +17,34 @@
   (setq inhibit-startup-message t)
   (setq use-gtags nil)
 
-  (load-file (concat jma-elisp-base "useful.el"))
-  (load-file (concat jma-elisp-base "mode-hooks.el"))
-  (load-file (concat jma-elisp-base "post.el"))
-  (load-file (concat jma-elisp-base "mutt-alias.el"))
-  (load-file (concat jma-elisp-base "dict.el"))
-  (load-file (concat jma-elisp-base "evan.el"))
-  (load-file (concat jma-elisp-base "jma.el"))
-  (load-file (concat jma-elisp-base "misc.el"))
-  ; copied from https://github.com/fxbois/web-mode.git
-  (load-file (concat jma-elisp-base "web-mode.el"))
-  (load-file (concat jma-elisp-base "pdftools.el"))
-  (load-file (concat jma-elisp-base "protobuf-mode.el"))
-  (load-file (concat jma-elisp-base "google-c-style.el"))
-  (load-file (concat jma-elisp-base "clang-format.el"))
-  (load-file (concat jma-elisp-base "puppet-mode.el"))
-  (load-file (concat jma-elisp-base "highlight-indentation.el"))
+  (if (file-exists-p jma-elisp-base)
+      (progn
+	(load-file (concat jma-elisp-base "useful.el"))
+	(load-file (concat jma-elisp-base "mode-hooks.el"))
+	(load-file (concat jma-elisp-base "post.el"))
+	(load-file (concat jma-elisp-base "mutt-alias.el"))
+	(load-file (concat jma-elisp-base "dict.el"))
+	(load-file (concat jma-elisp-base "evan.el"))
+	(load-file (concat jma-elisp-base "jma.el"))
+	(load-file (concat jma-elisp-base "misc.el"))
+	;; copied from https://github.com/fxbois/web-mode.git
+	(load-file (concat jma-elisp-base "web-mode.el"))
+	(load-file (concat jma-elisp-base "pdftools.el"))
+	(load-file (concat jma-elisp-base "protobuf-mode.el"))
+	(load-file (concat jma-elisp-base "google-c-style.el"))
+	(load-file (concat jma-elisp-base "clang-format.el"))
+	(load-file (concat jma-elisp-base "puppet-mode.el"))
+	(load-file (concat jma-elisp-base "highlight-indentation.el"))
 
-  ;; cf. https://github.com/spotify/dockerfile-mode
-  (load-file (concat jma-elisp-base "dockerfile-mode.el"))
-  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+	;; cf. https://github.com/spotify/dockerfile-mode
+	(load-file (concat jma-elisp-base "dockerfile-mode.el"))
+	(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+	))
+
+  ;; Downloaded and installed manually due to strange bug in
+  ;; emacs 26.1 install.
+  (load "auctex.el" nil t t)
+  (load "preview-latex.el" nil t t)
 
   ;; Set frame title so that ratpoison-gtd can record something about
   ;; what I'm doing.
