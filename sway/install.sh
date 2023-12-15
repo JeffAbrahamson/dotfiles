8a8ff0687b3eba4412908a3fe0dc6c9fe062cd1e
Jeff Abrahamson <jeff@p27.eu>
2023-12-15 21:54:22 +0100

    SQUASH ME

--- sway/install.sh
+++ sway/install.sh
@@ -6,14 +6,17 @@ dest=$HOME/.config/sway
 maybe_mkdir $dest
 maybe_mkdir $(dirname $dest)
 (cd sway/ && copy_to config $dest)
+swaylock_dest="$HOME/.swaylock"
+maybe_mkdir "$swaylock_dest"
+(cd sway/ && copy_to swaylock_config "$swaylock_dest/config")
 
 dest=$HOME/bin/
 maybe_mkdir $dest
 (
     (
 	cd sway/bin
-	for f in *; do
-	    copy_to $f $dest
-	done
+	# for f in *; do
+	#     copy_to $f $dest
+	# done
     )
 )
