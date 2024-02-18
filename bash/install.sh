2634c2d9a4797d3be5caa1efc37c3f576bf705aa
Jeff Abrahamson <jeff@p27.eu>
2024-02-18 02:52:57 +0100

    Refactor bash dotfiles to conf.d-style includes
    Rather than enumerating the specific files to include,
    just source what's in well-known configuration directories.

--- bash/install.sh
+++ bash/install.sh
@@ -4,12 +4,7 @@
 
 dest=$HOME/.dotfiles/bash/
 maybe_mkdir $dest
-(
-    cd bash
-    for f in *; do
-	copy_to $f $dest
-    done
-)
+rsync -av bash/ $dest/
 
 maybe_append .bashrc .dotfiles/bash/rc bashrc-include
 maybe_append .bash_profile .dotfiles/bash/profile bash_profile-include
