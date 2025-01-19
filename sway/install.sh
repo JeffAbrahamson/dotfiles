aae58c1a2e55bb92c5f26aae594a8a5f4ecf366d
Jeff Abrahamson <jeff@p27.eu>
2025-01-19 11:04:47 +0100

    Use kitty instead of sakura
    I picked sakura when I started using sway because the default choice
    in sway was foot, which bothered me for reasons I don't remember.
    
    Today I discovered kitty, which seems to be quite good.  It has a bit
    of a learning curve, on a par with urxvt, so the choice is tentative.
    But it already seems better than sakura.

--- sway/install.sh
+++ sway/install.sh
@@ -10,6 +10,10 @@ swaylock_dest="$HOME/.swaylock"
 maybe_mkdir "$swaylock_dest"
 (cd sway/ && copy_to swaylock_config "$swaylock_dest/config")
 
+dest=$HOME/.config/kitty
+maybe_mkdir $dest
+(cd sway/ && copy_to kitty.conf $dest)
+
 dest=$HOME/bin/
 maybe_mkdir $dest
 (
