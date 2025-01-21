61a2c6ef2a32ac1588f62f40ad2b5533ce9cbfc4
Jeff Abrahamson <jeff@p27.eu>
2025-01-21 21:14:10 +0100

    Provide font info for emacs under sway / XWayland
    Under XWayland, emacs will look at .Xdefaults rather than at
    .Xresources.

--- X11/install.sh
+++ X11/install.sh
@@ -2,21 +2,10 @@
 
 . ../script/lib.sh
 
-copy_to xsessionrc $HOME/.xsessionrc
-if [ "X$HOSTNAME" = Xstarshine ]; then
-    dpi=192
-    font="xft:DejaVuSansMono:size=10"
-elif [ "X$HOSTNAME" = Xvogel ]; then
-    dpi=144
-    font="xft:FreeMono:size=11"
+if [ "X$HOSTNAME" = Xvogel ]; then
+    # dpi=144
+    dpi=108
 else
     dpi=96
-    font="9x15"
 fi
-cat Xresources | sed -e "s/{% dpi %}/$dpi/; s/{% font %}/$font/;" > $HOME/.Xresources
-
-if type -p xrdb >/dev/null; then
-    xrdb $HOME/.Xresources
-else
-    echo "xrdb is absent, any Xresources changes will be delayed to next X start."
-fi
+cat Xdefaults  | sed -e "s/{% dpi %}/$dpi/;" > $HOME/.Xdefaults
