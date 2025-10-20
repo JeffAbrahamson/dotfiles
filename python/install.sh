38e38e968d7d83c3058f1290d1af0c21f1604246
Jeff Abrahamson <jeff@p27.eu>
2025-10-20 20:06:40 +0200

    Fix bandwidth_tool install

--- python/install.sh
+++ python/install.sh
@@ -11,3 +11,12 @@ rsync -av bash/ $bash_dest/
 np_dest=$HOME/.config/virtualenvs/new-project-template/
 maybe_mkdir "$np_dest"
 rsync -av --delete new-project-template/ "$np_dest/"
+
+site_packages=$(python3 - <<'PY'
+import site
+print(site.getusersitepackages())
+PY
+)
+
+maybe_mkdir "$site_packages/bandwidth_tool/"
+rsync -av --delete ./bandwidth_tool/ "$site_packages/bandwidth_tool/"
